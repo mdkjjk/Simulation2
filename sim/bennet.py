5f6f44912c2ccf85e596a7ac0a262a3a5cba6882
mizuki.I <186134191+mdkjjk@users.noreply.github.com>
2026-02-20 15:50:22 +0900

    2_16

--- sim/bennet.py
+++ sim/bennet.py
@@ -6,7 +6,7 @@ from netsquid.qubits import operators as ops
 from netsquid.qubits import qubitapi as qapi
 from netsquid.qubits import ketstates as ks
 from netsquid.qubits.ketutil import outerprod
-from netsquid.qubits.ketstates import s0, s1
+from netsquid.qubits.ketstates import s0, s1, s00, b11
 from netsquid.qubits.state_sampler import StateSampler
 from netsquid.qubits.qformalism import QFormalism
 from netsquid.qubits.dmtools import DenseDMRepr
@@ -40,12 +40,38 @@ class Bennet(NodeProtocol):
     """
 
 
-def network_setup(source_delay=1e5, node_distance=100, depolar_rate=1000):
+def network_setup(source_delay=1e5, source_fidelity_sq=0.8, node_distance=100, depolar_rate=1000):
     network = Network("bennet_network")
 
     node_a, node_b = network.add_nodes(["node_A", "node_B"])
     node_a.add_subcomponent(QuantumProcessor("QuantumMemory_A", num_positions=11,
         fallback_to_nonphysical=True))   # パラメータ「memory_noise_models」によりメモリ滞在によるノイズの影響を設定可能
-    state = DenseDMRepr(np.array[[(1-f)/3, 0, 0, 0], [0, (2*f+1)/6, (1-4*f)/6, 0],
-                                 [0, (1-4*f)/6, (2*f+1)/6, 0], [0, 0, 0, (1-f)/3]])
-    state_sampler = StateSampler
+    #state = DenseDMRepr(np.array[[(1-f)/3, 0, 0, 0], [0, (2*f+1)/6, (1-4*f)/6, 0],
+                                 #[0, (1-4*f)/6, (2*f+1)/6, 0], [0, 0, 0, (1-f)/3]])   werner状態の密度行列
+    state_sampler = StateSampler([ks.b11, ks.s00], probabilities=[source_fidelity_sq, 1 - source_fidelity_sq])
+    node_a.add_subcomponent(QSource("QSource_A", state_sampler=state_sampler,
+        models={"emission_delay_model": FixedDelayModel(delay=source_delay)},
+        num_ports=2, status=SourceStatus.EXTERNAL))
+    node_b.add_subcomponent(QuantumProcessor("QuantumMemory_B", num_positions=11,
+        fallback_to_nonphysical=True))   # パラメータ「memory_noise_models」によりメモリ滞在によるノイズの影響を設定可能
+    
+    conn_cchannel = DirectConnection("CChannelConn_AB",
+        ClassicalChannel("CChannel_A->B", length=node_distance, models={"delay_model": FibreDelayModel(c=200e3)}),
+        ClassicalChannel("CChannel_B->A", length=node_distance, models={"delay_model": FibreDelayModel(c=200e3)}))
+    network.add_connection(node_a, node_b, connection=conn_cchannel)
+    qchannel = QuantumChannel("QChannel_A->B", length=node_distance,
+                              models={"quantum_noise_model": DepolarNoiseModel(depolar_rate),
+                                      "delay_model": FibreDelayModel(c=200e3)})
+    port_name_a, port_name_b = network.add_connection(
+        node_a, node_b, channel_to=qchannel, label="quantum")
+    
+    # Link Alice ports:
+    node_a.subcomponents["QSource_A"].ports["qout1"].forward_output(
+        node_a.ports[port_name_a])
+    node_a.subcomponents["QSource_A"].ports["qout0"].connect(
+        node_a.qmemory.ports["qin0"])
+    # Link Bob ports:
+    node_b.ports[port_name_b].forward_input(node_b.qmemory.ports["qin0"])
+    return network
+
+network = network_setup()
